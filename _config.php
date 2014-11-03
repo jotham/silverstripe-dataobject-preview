953120b36e1ef295f04b87cfde64232d2289ebb7
Jotham Read <jotham.read@gmail.com>
2014-11-03 20:08:30 +1300

    Fixed up example

--- _config.php
+++ _config.php
@@ -1,5 +1,7 @@
 <?PHP
 
+GridFieldDetailForm_ItemRequest::add_extension('PreviewableDataObjectExt');
+
 // TODO: We should be able to do this through the YAML instead of this hack?
 $module_prefix = basename(__DIR__);
 $items = Config::inst()->get('ModelAdmin', 'extra_requirements_javascript');
---
+++ example_files/PreviewPage.ss
@@ -0,0 +1,6 @@
+<% include SideBar %>
+<div class="content-container">
+   Example Preview
+   <h1>This is object $Name</h1>
+   $Form
+</div>
