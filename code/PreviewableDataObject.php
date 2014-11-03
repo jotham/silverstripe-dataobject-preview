57fc55cadbafe4b20c0798383d8f09dd5f5b95a7
Jotham Read <jotham.read@gmail.com>
2014-11-03 17:29:07 +1300

    Tidied some things up, fixed some bugs

--- code/PreviewableDataObject.php
+++ code/PreviewableDataObject.php
@@ -1,54 +1,67 @@
 <?PHP
 
+interface CanPreviewableDataObject {
+   /*
+    * Return the Link to display the PreviewableDataObject
+    *
+    * @param SiteTree $page First SiteTree object we found for this controller
+    * @param PreviewableDataObject $obj The object we want to display on the page
+    *
+    * @return string Link to route for displaying object
+    *
+    * Example:
+    *
+    * TODO: Provide a base class rather than an interface for convenience?
+    */
+   public function getPreviewAction(SiteTree $page, PreviewableDataObject $obj);
+}
+
 class PreviewableDataObject extends DataObject implements CMSPreviewable {
 
-   static $preview_class = 'OtherProductPage';
+   /*
+    * This is the Page we will use to render the PreviewableDataObject.
+    * Note: Currently we assume that [PreviewPage]_Controller also exists.
+    */
+   static $preview_page = '[PreviewPage]';
 
-   public function Link($action = null) {
-      trace('PreviewableDataObject::Link', $action);
-      return 'http://staging.fbcloud.co.nz/doap-test/admin/other_products/OtherProduct/EditForm/field/OtherProduct/item/2/edit';
-      return Controller::join_links(Director::baseURL(), $this->RelativeLink($action));
+   public function PreviewLink(){
+      /*
+       * This is the link we show in the CMS Preview panel
+       */
+      list($page_cls, $controller_cls) = $this->getPreviewClasses();
+      return $controller_cls::getPreviewAction($page_cls::get()->First(), $this);
    }
 
-   public function RelativeLink($action = null){
-      trace('PreviewableDataObject::RelativeLink', $action);
-      trace('RelativeLink', Controller::curr()->Link());
+   public function CMSEditLink(){
+      /*
+       * This is used by SilverStripeNavigator to provide an edit page link.
+       * TODO: Possibly integrate Model Admin association for CMSEditLink
+       */
+      error_log('PreviewableDataObject->CMSEditLink - Not implemented.');
+      return $this->Link();
+   }
+
+   /*
+    * Both of these depend on us knowing the context of the request for this object
+    * TODO: Define if editor context or front end website context.
+    */
+   public function Link($action = null) {
+      return Controller::join_links(Director::baseURL(), $this->RelativeLink($action));
+   }
+   public function RelativeLink(){
       return '';
    }
 
-   public function PreviewLink(){
-      return $this->getPreviewClass()->AbsoluteLink('show/'.$this->ID);
-   }
-
-   public function CMSEditLink($action = null) {
-      trace('PreviewableDataObject::CMSEditLink', $action);
-      return $this->Link($action);
-   }
-
-   public function getCMSFields(){
-      error_log('PreviewableDataObject::getCMSFields');
-      $fields = parent::getCMSFields();
-      return $fields;
-   }
-
-   public function getPreviewClass(){
-      $previewClass = $this->stat('preview_class');
-      $controllerClass =  $previewClass . "_Controller";
-      if( Controller::curr() instanceof $controllerClass ) {
-         $previewPage = Controller::curr();
-      } else {
-         $previewPage = $previewClass::get()->First();
+   private function getPreviewClasses(){
+      $cls = $this->stat('preview_page');
+      $controller_cls = $cls . '_Controller';
+      if ( !class_exists($cls) ) {
+         throw new Exception('Class does not exist: ' . $cls);
       }
-      return $previewPage;
-   }
-
-}
-
-class PreviewableModelAdmin extends ModelAdmin {
-
-   public function getEditForm($id = null, $fields = null) {
-      $form = parent::getEditForm($id, $fields);
-      return $form;
+      if ( !in_array('CanPreviewableDataObject', class_implements($controller_cls)) ) {
+         throw new Exception('Class does not implement CanPreviewableDataObject: ' . $controller_cls);
+      }
+      return array($cls, $controller_cls);
    }
 
 }
@@ -57,40 +70,21 @@ class PreviewableDataObjectExt extends Extension {
 
    public function updateItemEditForm(&$form){
       $fields = $form->Fields();
-      if( !$fields->fieldByName('SilverStripeNavigator')) {
-         $navigator = new SilverStripeNavigator($this->owner->record);
-         $field = new LiteralField(
-            'SilverStripeNavigator',
-            $navigator->renderWith('LeftAndMain_SilverStripeNavigator')
-         );
-         $field->setAllowHTML(true);
-         $fields->push($field);
+      if( $this->owner->record instanceof PreviewableDataObject && !$fields->fieldByName('SilverStripeNavigator') ) {
+         $this->injectNavigatorAndPreview($form, $fields);
       }
+   }
+
+   private function injectNavigatorAndPreview(&$form, &$fields){
+      $editForm = $fields->fieldByName('EditForm');
+      //TODO: Do we need to verify we are in the right controller?
+      $template = Controller::curr()->getTemplatesWithSuffix('_SilverStripeNavigator');
+      $navigator = new SilverStripeNavigator($this->owner->record);
+      $field = new LiteralField('SilverStripeNavigator', $navigator->renderWith($template));
+      $field->setAllowHTML(true);
+      $fields->push($field);
       $form->addExtraClass('cms-previewable');
       $form->removeExtraClass('cms-panel-padded center');
    }
 
-   public function updateEditForm(&$form) {
-      /*$fields = $form->Fields();
-      trace($this->sanitiseClassName($this->owner->modelClass));
-      trace($this->owner->modelClass);
-      [>!dd($fields->fieldByName($this->sanitiseClassName($this->owner->modelClass))->getState(false));<]
-      trace($this->owner->getAction());
-      return;
-      if ( Controller::curr()->getAction() == 'EditForm' ) { // SS suggests this is a special name not subject to immediate change
-         if( !$fields->fieldByName('SilverStripeNavigator')) {
-            $navigator = new SilverStripeNavigator($page);
-            trace($this->getTemplatesWithSuffix('_SilverStripeNavigator'));
-            $navField = new LiteralField(
-               'SilverStripeNavigator',
-               $navigator->renderWith($this->getTemplatesWithSuffix('_SilverStripeNavigator'))
-            );
-            $navField->setAllowHTML(true);
-            $fields->push($navField);
-         }
-         trace('Enabling preview view');
-         $form->addExtraClass('cms-previewable');
-         $form->removeExtraClass('cms-panel-padded center');
-      }*/
-   }
 }
---
+++ templates/Layout/PreviewableDataObjectEmpty.ss
@@ -0,0 +1,2 @@
+<!-- Placeholder when we have no DO to show -->
+<style type="text/css">body { background-color: #e6eaed; }</style>
